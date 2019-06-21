ffb76c53c01b3aa4d28391597fb0599eddde42c8
beatrizsmerino <beatrizsmerino@gmail.com>
2019-06-21 21:02:46 +0200

    Added router: Home, Git flow and Bibliografy

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app" v-cloak>
+		<page-header></page-header>
+
+		<page-title></page-title>
+
+		<router-view/>
+	</div>
 </template>
 
+
+<script>
+  import PageHeader from './components/PageHeader.vue'
+  import PageTitle from './components/PageTitle.vue'
+  
+  export default {
+    components: {
+      PageHeader,
+      PageTitle,
+    }
+  };
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	[v-cloak] {
+		display: none;
+	}
+
+	*,
+	*:after,
+	*:before {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+  body {
+    padding-top: 6rem;
+    -webkit-font-smoothing: antialiased;
+		font-family: "Oxygen", Helvetica, sans-serif;
+		font-size: 62.5%;
+    color: #2c3e50;
+  }
+
+  a {
+		text-decoration: none;
+	}
 </style>
