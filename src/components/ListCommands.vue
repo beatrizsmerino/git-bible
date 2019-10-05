f09911cff58089877bc5a65bec7a1dc729f86053
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-05 23:33:12 +0200

    Added command to the 'list-commands' component with the data object.

--- src/components/ListCommands.vue
+++ src/components/ListCommands.vue
@@ -1,14 +1,61 @@
 <template>
-  <div class="list-commands"></div>
+  <ul class="list-commands">
+    <li v-for="command in commands" :key="command.id" class="command">
+      <p class="command__description">{{command.description}}</p>
+      <code class="command__code">{{command.code}}</code>
+    </li>
+  </ul>
 </template>
 
 
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      commands: [
+        {
+          id: 1,
+          code: "git --version",
+          description: "Known the git version"
+        },
+        {
+          id: 2,
+          code: "git init",
+          description: "Inizialice git repository"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 
 
 <style lang="scss" scoped>
+.list-commands {
+  width: 80%;
+  margin: 0 auto;
+  list-style: none;
+}
+
+.command {
+  padding: 1.2rem 2rem;
+  font-size: 1.2rem;
+  background-color: $color-light;
+  border-bottom: 0.5rem solid $color-silver;
+
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+
+  &__description {
+    color: $color-brand-2;
+  }
+
+  &__code {
+    margin-left: 1.5rem;
+    color: $color-brand-3;
+  }
+}
 </style>
