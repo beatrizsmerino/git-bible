5bf757c8d5273690e1c69b39ef21a85b5d8882bb
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-07 00:48:31 +0200

    Added 'PageFooter' component

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,34 @@
+<template>
+	<footer class="footer">
+		<div>
+			<p>© Copyright 2019. Git Bible. beatrizsmerino</p>
+		</div>
+
+		<div>
+			<nav class="nav">
+				<router-link to="/bibliografy" class="nav__item">Bibliografy</router-link>
+			</nav>
+		</div>
+	</footer>
+</template>
+
+
+<script>
+export default {
+
+};
+</script>
+
+
+<style lang="scss" scoped>
+.footer {
+	padding: 1rem 2rem;
+	display: flex;
+	justify-content: space-between;
+	color: $color-white;
+	background-color: $color-brand-2-light;
+}
+a {
+	color: $color-brand-2;
+}
+</style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,13 +1,12 @@
 <template>
-  <header class="page-header">
-    <div class="page-header__icon icon icon-git"></div>
-    <nav class="nav">
-      <router-link to="/" class="nav__item">Home</router-link>
-      <router-link to="/commands" class="nav__item">Commands</router-link>
-      <router-link to="/gitflow" class="nav__item">Git Flow</router-link>
-      <router-link to="/bibliografy" class="nav__item">Bibliografy</router-link>
-    </nav>
-  </header>
+	<header class="page-header">
+		<div class="page-header__icon icon icon-git"></div>
+		<nav class="nav">
+			<router-link to="/" class="nav__item">Home</router-link>
+			<router-link to="/commands" class="nav__item">Commands</router-link>
+			<router-link to="/gitflow" class="nav__item">Git Flow</router-link>
+		</nav>
+	</header>
 </template>
 
 
@@ -20,42 +19,42 @@ export default {};
 
 <style lang="scss" scoped>
 .page-header {
-  width: 100%;
-  height: 6rem;
-  padding: 1.5rem 3rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 999;
-  background-color: $color-brand-1;
+	width: 100%;
+	height: 6rem;
+	padding: 1.5rem 3rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 999;
+	background-color: $color-brand-1;
 
-  &__item {
-    display: flex;
-  }
+	&__item {
+		display: flex;
+	}
 
-  &__icon {
-    font-size: 4rem;
-    color: $color-white;
-  }
+	&__icon {
+		font-size: 4rem;
+		color: $color-white;
+	}
 }
 
 .nav {
-  font-size: 1.3rem;
-  font-weight: 600;
+	font-size: 1.3rem;
+	font-weight: 600;
 
-  &__item {
-    color: $color-brand-2;
+	&__item {
+		color: $color-brand-2;
 
-    &:not(:last-child) {
-      margin-right: 2rem;
-    }
+		&:not(:last-child) {
+			margin-right: 2rem;
+		}
 
-    &.router-link-exact-active {
-      color: $color-white;
-    }
-  }
+		&.router-link-exact-active {
+			color: $color-white;
+		}
+	}
 }
 </style>
