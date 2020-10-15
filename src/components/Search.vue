874579d0f09be888e48da8d70cb89cff1ac01d52
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-10-15 07:59:56 +0200

    Created search-results component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,14 +6,34 @@
 				<i class="form__icon icon icon-search"></i>
 			</div>
 		</form>
+		<div class="search-results">
+			<ul class="search-results__list">
+				<li v-for="command in commands" :key="command.id" class="search-results__item">
+					<router-link :to="`/commands/${command.id}`" class="search-results__link">
+						{{command.description}}
+					</router-link>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 
 
 <script>
+	import dataCommands from "../data/data-commands.json";
+
 	export default {
 		name: 'Search',
+		data() {
+			return {
+				commands: [],
+			}
+		},
+		created(){
+			this.commands = dataCommands;
+			console.table(this.commands);
+		}
 	};
 </script>
 
@@ -46,4 +66,39 @@
 		}
 	}
 
+	.search-results{
+		display: none;
+		max-width: 800px;
+		height: #{(6 * 3)}rem;
+		margin-top: 2rem;
+		font-size: 1.7rem;
+		overflow-y: scroll;
+
+		&__list{
+			list-style: none;
+		}
+
+		&__item{
+			height: 6rem;
+			text-align: left;
+		}
+
+		&__link{
+			width: 100%;
+			padding: 1.3rem;
+			color: $color-brand-3;
+			background-color: $color-light;
+			border-bottom: 0.5rem solid $color-silver;
+
+			&:hover{
+				background-color: $color-silver;
+
+				.search-results{
+					&__link{
+						color: $color-brand-2;
+					}
+				}
+			}
+		}
+	}
 </style>
