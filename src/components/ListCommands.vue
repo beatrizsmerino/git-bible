d0f909b89c9a3a5ee1baf2ca527a4b616cbaa492
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-10-19 03:17:50 +0200

    Created Command and ListCommand components

---
+++ src/components/ListCommands.vue
@@ -0,0 +1,59 @@
+<template>
+	<ul class="list-commands">
+		<li class="list-commands__item" v-for="command in commands" :key="command.id" :ref="command.name" :id="command.name" :data-index="command.id">
+			<command :dataCommand="command"/>
+		</li>
+	</ul>
+</template>
+
+
+
+<script>
+	import Command from "@/components/Command.vue";
+
+	export default {
+		name: 'ListCommands',
+		components: {
+			Command
+		},
+		props: {
+			commands: Object,
+		},
+		data() {
+			return {
+				anchorName: this.$route.hash.split('#').pop(),
+			};
+		},
+		methods: {
+			scrollAnchor: function($thisAnchor) {
+				setTimeout(() => {
+					if($thisAnchor !== undefined){
+						let $thisAnchorTop = $thisAnchor[0].offsetTop;
+						window.scrollTo({
+							left: 0,
+							top: $thisAnchorTop - 90 - 70,
+							behavior: 'smooth',
+						});
+						$thisAnchor[0].classList.add("is-selected");
+					}
+				}, 100);
+			}
+		},
+		mounted(){
+			this.scrollAnchor(this.$refs[this.anchorName]);
+		},
+	};
+</script>
+
+
+<style lang="scss" scoped>
+	.list-commands {
+		list-style: none;
+
+		&__item{
+			&:not(:last-child){
+				margin-bottom: 3rem;
+			}
+		}
+	}
+</style>
--- src/components/ListCommandsGit.vue
+++ src/components/ListCommandsGit.vue
@@ -1,56 +1,25 @@
 <template>
-	<ul class="list-commands">
-		<li v-for="command in commands" :key="command.id" :ref="command.name" :id="command.name" class="command" :data-index="command.id">
-			<h3 class="command__title">
-				{{command.title}}
-			</h3>
-			<div class="command__description">
-				<p>
-					{{command.description}}
-				</p>
-			</div>
-			<code-theme class="command__code" :code="command.code"/>
-		</li>
-	</ul>
+	<list-commands :commands="commands"/>
 </template>
 
 
 
 <script>
-	import CodeTheme from "@/components/CodeTheme.vue";
+	import ListCommands from "@/components/ListCommands.vue";
 	import dataCommandsGit from "../data/data-commands-git.json";
 
 	export default {
 		name: 'ListCommandsGit',
 		components: {
-			CodeTheme
+			ListCommands
 		},
 		data() {
 			return {
-				anchorName: this.$route.hash.split('#').pop(),
 				commands: []
 			};
 		},
-		methods: {
-			scrollAnchor: function($thisAnchor) {
-				setTimeout(() => {
-					if($thisAnchor !== undefined){
-						let $thisAnchorTop = $thisAnchor[0].offsetTop;
-						window.scrollTo({
-							left: 0,
-							top: $thisAnchorTop - 90 - 70,
-							behavior: 'smooth',
-						});
-						$thisAnchor[0].classList.add("is-selected");
-					}
-				}, 100);
-			}
-		},
 		created() {
 			this.commands = dataCommandsGit;
-		},
-		mounted(){
-			this.scrollAnchor(this.$refs[this.anchorName]);
 		},
 	};
 </script>
@@ -58,37 +27,5 @@
 
 
 <style lang="scss" scoped>
-	.list-commands {
-		list-style: none;
-	}
-
-	.command {
-		padding: 1.92rem 3.2rem;
-		font-size: 1.92rem;
-		background-color: $color-light;
-		border-bottom: 0.8rem solid $color-silver;
-
-		&:not(:last-child) {
-			margin-bottom: 1.6rem;
-		}
-
-		&__title{
-			margin-bottom: 0.5rem;
-			color: $color-brand-2;
-		}
-
-		&__description {
-			margin-bottom: 2rem;
-			color: $color-brand-2;
-		}
-
-		&__code {
-			width: 100%;
-			display: inline-block;
-		}
-
-		&.is-selected{
-			background-color: $color-silver;
-		}
-	}
+	
 </style>
