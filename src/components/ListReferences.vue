d39c4c0921afa4321cb11adcf0b032bd297159a3
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-06 13:07:59 +0100

    Refactoring. Separated Reference component to the ListReferences

--- src/components/ListReferences.vue
+++ src/components/ListReferences.vue
@@ -1,29 +1,12 @@
 <template>
 	<ul class="list-references">
 		<li
+			class="list-references__item"
 			v-for="(reference, index) in references"
 			:key="`reference-${index}`"
-			class="reference"
 			:data-index="index"
 		>
-			<a
-				class="reference__link"
-				:href="reference.url"
-				target="_blank"
-			>
-				<p class="reference__description">
-					{{reference.description}}
-				</p>
-				<p class="reference__url">
-					<i class="reference__url-icon">
-						<font-awesome-icon :icon="['fas', 'link']" />
-					</i>
-
-					<span class="reference__url-text text-ellipsis">
-						{{reference.url}}
-					</span>
-				</p>
-			</a>
+			<reference :dataReference="reference" />
 		</li>
 	</ul>
 </template>
@@ -32,9 +15,14 @@
 
 <script>
 	import data from "../data/data-references.json";
+	import Reference from "@/components/Reference.vue";
+
 
 	export default {
 		name: 'ListReferences',
+		components: {
+			Reference
+		},
 		data() {
 			return {
 				references: []
@@ -51,48 +39,11 @@
 <style lang="scss" scoped>
 	.list-references {
 		list-style: none;
-	}
 
-	.reference {
-		width: 100%;
-		display: flex;
-		align-items: center;
-		font-size: 1.92rem;
-		background-color: $color-light;
-
-		&:hover {
-			background-color: $color-silver;
-
-			.reference__link {
-				color: $color-brand-2;
+		&__item {
+			&:not(:last-child) {
+				margin-bottom: 1.6rem;
 			}
-		}
-
-		&:not(:last-child) {
-			margin-bottom: 1.6rem;
-		}
-
-		&__link {
-			width: 100%;
-			padding: 1.92rem 3.2rem;
-			color: $color-brand-3;
-		}
-
-		&__url {
-			display: flex;
-			align-items: center;
-		}
-
-		&__url-icon {
-			margin-right: 0.8rem;
-		}
-
-		&__url-text {
-			max-width: 120rem;
-		}
-
-		&__description {
-			color: $color-brand-2;
 		}
 	}
 </style>
---
+++ src/components/Reference.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="reference">
+		<a
+			class="reference__link"
+			:href="dataReference.url"
+			target="_blank"
+		>
+			<p class="reference__description">
+				{{dataReference.description}}
+			</p>
+			<p class="reference__url">
+				<i class="reference__url-icon">
+					<font-awesome-icon :icon="['fas', 'link']" />
+				</i>
+
+				<span class="reference__url-text text-ellipsis">
+					{{dataReference.url}}
+				</span>
+			</p>
+		</a>
+	</div>
+</template>
+
+
+
+<script>
+	export default {
+		name: 'Reference',
+		props: {
+			dataReference: Object
+		}
+	};
+</script>
+
+
+
+<style lang="scss" scoped>
+	.reference {
+		width: 100%;
+		display: flex;
+		align-items: center;
+
+		&__link {
+			width: 100%;
+			padding: 1.92rem 3.2rem;
+			font-size: 1.92rem;
+			color: $color-brand-3;
+			background-color: $color-light;
+
+			&:hover {
+				color: $color-brand-2;
+				background-color: $color-silver;
+			}
+		}
+
+		&__description {
+			color: $color-brand-2;
+		}
+
+		&__url {
+			display: flex;
+			align-items: center;
+		}
+
+		&__url-icon {
+			margin-right: 0.8rem;
+		}
+
+		&__url-text {
+			max-width: 120rem;
+		}
+	}
+</style>
