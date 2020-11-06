05e1311f2214d73fdda4b068b2b77bc6f2bc18ad
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-06 11:09:43 +0100

    Bug fixed. Added reset and fonts to the App. The styles could not be overwritten.

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,14 @@
 			PageTitle,
 			PageFooter
 		},
+		methods: {
+			addSticky: function () {
+				const html = document.getElementsByTagName('html')[0];
+				const body = document.getElementsByTagName('body')[0];
+				html.classList.add("sticky");
+				body.classList.add("sticky__body");
+			}
+		},
 		watch: {
 			$route: {
 				handler(to, from) {
@@ -41,36 +49,95 @@
 				immediate: true,
 			}
 		},
+		created() {
+			this.addSticky();
+		},
 	};
 </script>
 
 
+
 <style lang="scss">
+	@import "./assets/scss/base/_base-reset.scss";
+	@import "./assets/scss/base/_base-fonts.scss";
+
 	[v-cloak] {
 		display: none;
 	}
 
 	body {
-		font-family: "Oxygen", sans-serif;
+		font-family: $font-oxygen;
 		color: $color-gray;
 	}
 
-	// STICKY
-	html,
-	body {
-		height: 100%;
+	// SELECTION TEXT
+	// =================================================
+	::-moz-selection {
+		/* Code for Firefox */
+		color: $color-brand-1;
+		background: $color-brand-2;
 	}
 
+	::selection {
+		color: $color-brand-1;
+		background: $color-brand-2;
+	}
+
+	// FORMS
+	// =================================================
+	::-webkit-input-placeholder {
+		/* Chrome/Opera/Safari */
+		font-weight: 400;
+		color: $color-brand-2-light;
+	}
+
+	::-moz-placeholder {
+		/* Firefox 19+ */
+		font-weight: 400;
+		color: $color-brand-2-light;
+	}
+
+	:-ms-input-placeholder {
+		/* IE 10+ */
+		font-weight: 400;
+		color: $color-brand-2-light;
+	}
+
+	:-moz-placeholder {
+		/* Firefox 18- */
+		font-weight: 400;
+		color: $color-brand-2-light;
+	}
+
+	// TEXT
+	// =================================================
+	.text-ellipsis {
+		max-width: 60rem;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		overflow: hidden;
+	}
+
+	// STICKY
+	// =================================================
 	.sticky {
+		height: 100%;
+
+		&__body {
+			height: 100%;
+		}
+
 		&__app {
 			height: 100%;
 			display: flex;
 			flex-direction: column;
 		}
+
 		&__content {
 			margin-top: 9rem;
 			flex: 1 0 auto;
 		}
+
 		&__footer {
 			flex-shrink: 0;
 		}
