de242d59193e1671120c6d4f941c512bb66bbb92
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-10-30 01:17:52 +0200

    BUG FIXED. Found and fix errors of eslint
    - error: Use the 'u' flag (require-unicode-regexp) at src/router/index.js:98:8:
    - error: Expected to return a value at the end of arrow function (consistent-return) at src/router/index.js:133:36:
    - error: This line has 2 statements. Maximum allowed is 1 (max-statements-per-line) at src/router/index.js:154:26:
    - error: This line has 2 statements. Maximum allowed is 1 (max-statements-per-line) at src/router/index.js:160:26:
    - error: Expected to return a value at the end of arrow function (consistent-return) at src/router/index.js:133:36:
    - error: This line has 2 statements. Maximum allowed is 1 (max-statements-per-line) at src/router/index.js:154:26:
    - Unexpected use of undefined (no-undefined) at src/App.vue:40:19:
    - warning: Expected an assignment or function call and instead saw an expression (no-unused-expressions) at src/App.vue:65:5:
    - warning: Variable 'handleOutsideClick' should be initialized on declaration (init-declarations) at src/components/FilterNav.vue:59:6:
    - warning: Expected an assignment or function call and instead saw an expression (no-unused-expressions) at src/components/Page/PageNav.vue:100:5:

--- src/router/index.js
+++ src/router/index.js
@@ -96,7 +96,7 @@ const scrollBehavior = function (to, from, savedPosition) {
 
 			const selectorIndex = document.querySelector(to.hash).getAttribute('data-index');
 
-			if (/^#\d/.test(to.hash) || document.querySelector(to.hash)) {
+			if (/^#\d/u.test(to.hash) || document.querySelector(to.hash)) {
 				if (selectorIndex) {
 					
 					if (selectorIndex == 0) {
@@ -143,13 +143,17 @@ router.beforeEach((to, from, next) => {
 	from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
 
 	// If a route with a title was found, set the document (page) title to that value.
-	if (nearestWithTitle) document.title = `${nearestWithTitle.meta.title} | GitBible` || 'GitBible';
+	if (nearestWithTitle) {
+		document.title = `${nearestWithTitle.meta.title} | GitBible` || 'GitBible';
+	}
 
 	// Remove any stale meta tags from the document using the key attribute we set below.
 	Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
 
 	// Skip rendering meta tags if there are none.
-	if (!nearestWithMeta) return next();
+	if (!nearestWithMeta) {
+		return next();
+	}
 
 	// Turn the meta tag definitions into actual elements in the head.
 	nearestWithMeta.meta.metaTags.map(tagDef => {
