3cb6347fcd147a80de5c9cef928af1f79734b8e2
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-11-08 03:59:52 +0100

    BUG FIXED. Wrong vue configuration for run npm script ‘lint' on save the files

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? '/git-bible/' : '/',
-	lintOnSave: false,
+	lintOnSave: true,
 	css: {
 		loaderOptions: {
 			sass: {
 				data: `@import "@/assets/scss/_styles.scss";`
 			}
 		}
+	},
+	chainWebpack: config => {
+		config.module.rule('eslint').use('eslint-loader').
+			options({
+				fix: true
+			});
 	}
 };
